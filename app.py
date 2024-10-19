309906c2551e26e0edd5fa7b3577ef55c68b600e
Varun Penumudi <73636316+varunpenumudi@users.noreply.github.com>
2024-10-19 10:07:23 +0530

    Added hello poem

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+hello_str = """
+Hello to you, my friend so dear,
+With open hearts, I bring you cheer!
+A brand new day, a bright hello,
+Like sunshine’s rays, may kindness grow.
+
+Through every word and every smile,
+Let joy remain, if just a while.
+So here’s my greeting, warm and true,
+A simple “hi” from me to you!
+"""
+print(hello_str)
